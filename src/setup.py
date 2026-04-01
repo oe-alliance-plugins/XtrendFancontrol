e216277b4bef9f7fe3c2baa855a9c895f03da404
Your Name <you@example.com>
2026-04-01 03:52:25 +0000

    Adapt repository layout and template files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setup_translate
+
+pkg = 'SystemPlugins.XtrendFancontrol'
+setup(name='enigma2-plugin-systemplugins-fancontrol',
+       version='3.0',
+       description='Fancontrol Settings inStandby Mode',
+       package_dir={pkg: 'XtrendFancontrol'},
+       packages=[pkg],
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass=setup_translate.cmdclass,  # for translation
+      )
